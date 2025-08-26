320f8eb9730267155cb9a5f12c56ec50a9cf9b5a
ATUL RAJ <160324865+atulraj2912@users.noreply.github.com>
2025-08-26 18:58:08 +0000

    conversions and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,3 +26,40 @@ let someNumber=33
 let stringNumber=String(someNumber);
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+//*************OPERATIONS*************************/
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(2%2);
+
+
+let str1="hello"
+let str2=" atul"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2+2)//122
+console.log(1+"2")//12
+console.log("1"+"2") //12
+console.log(1+2+"2")//32
+
+console.log(+true); //1
+console.log(+"");//0
+
+let num1,num2,num3
+num1=num2=num3=2+2 //4
+
+let gameCounter=100
+gameCounter++;//postfix increment
+console.log(gameCounter);//101
+++gamecounter;//prefix increment
+console.log(gameCounter);//101
