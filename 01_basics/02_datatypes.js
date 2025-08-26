021aae2f33dbeadad0c59d424f50c0ca7123338a
ATUL RAJ <160324865+atulraj2912@users.noreply.github.com>
2025-08-26 18:29:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //we are using node.js not browser
+console.log(3+3)
+console.log("Atul")  // ; is avoided in new standards
+let name="Atul"
+let age=18
+let isloggedin=false
+let state=null
+//number
+//bigint
+//string=> ""
+//boolean=>true/false
+//null=>standalone value
+//undefined =>
+//symbol=> to find uniquness
+// oblject
+
+console.log(typeof age)//number
+console.log(typeof null)//object
+console.log(typeof undefined) //undefined
+
